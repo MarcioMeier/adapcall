cf9f35d3c6fbcf4bf0f6fa5c9076977961a571cb
Márcio Meier <marcio@adapcon.com.br>
2020-01-27 07:45:32 -0300

    ligacões

---
+++ src/components/FormLigacao.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <form @submit.prevent="criarLigacao">
+      <fieldset>
+        <legend>Criar Ligação</legend>
+
+        <div>
+          Solicitante
+          <input type="text"
+          v-model="ligacao.solicitante"
+          >
+        </div>
+        
+        <div>
+          Solicitado
+          <input type="text"
+          v-model="ligacao.solicitado"
+          >
+        </div>
+        
+        <div>
+          Cliente
+          <input type="text"
+          v-model="ligacao.cliente"
+          >
+        </div>
+        
+        <div>
+          Departamento
+          <input type="text"
+          v-model="ligacao.departamento"
+          >
+        </div>
+        
+        <div>
+          Urgente?
+          <input type="checkbox"
+          v-model="ligacao.urgente"
+          >
+        </div>
+
+        <br/>
+        <div>
+          <button type="submit">Adicionar</button>
+          &nbsp;
+          <button type="button" @click="apagarLista">Apagar Lista</button>
+        </div>
+
+      </fieldset>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ligacao: this.novaLigacao(),
+      lastId: 2525,
+    };
+  },
+  methods: {
+    criarLigacao() {
+      this.$emit('adicionar-ligacao', {
+        ...this.ligacao,
+        id: (++this.lastId)}
+      );
+      this.ligacao=this.novaLigacao();
+    },
+    novaLigacao() {
+      return {
+        solicitante: '',
+        solicitado: '',
+        cliente: '',
+        departamento: '',
+        urgente: false,
+      }
+    },
+    apagarLista() {
+      this.$emit('apagar-lista');
+    }
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Ligacoes.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <table v-if="ligacoes.length">
+      <tr>
+        <th>ID</th>
+        <th>Solicitante</th>
+        <th>Solicitado</th>
+        <th>Cliente</th>
+        <th>Departamento</th>
+        <th>Urgente?</th>
+      </tr>
+      <tr v-for="ligacao in ligacoes" :key="ligacao.id">
+        <td>{{ ligacao.id }}</td>
+        <td>{{ ligacao.solicitante }}</td>
+        <td>{{ ligacao.solicitado }}</td>
+        <td>{{ ligacao.cliente }}</td>
+        <td>{{ ligacao.departamento }}</td>
+        <td>{{ ligacao.urgente ? 'Sim' : 'Não' }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['ligacoes']
+}
+</script>
+
+<style>
+
+</style>
